40b647ade09754e6f2d11e3d90ab84866480c86d
Christian Baker <cbaker87@gmail.com>
2022-01-12 23:54:49 +0000

    disable secureRoute until production

--- server/config/router.js
+++ server/config/router.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import players from '../controllers/players.js'
 import auth from '../controllers/auth.js'
-import secureRoute from '../lib/secureRoute.js'
+// import secureRoute from '../lib/secureRoute.js'
 import games from '../controllers/games.js'
 
 const router = express.Router()
@@ -11,17 +11,22 @@ router.route('/games')
   .post(games.create)
 
 router.route('/players')
-  .get(secureRoute, players.index)
-  .post(secureRoute, players.create)
+  .get(players.index)
+  .post(players.create)
 
 router.route('/players/:playerId')
-  .get(secureRoute, players.show)
-  .put(secureRoute, players.update)
-  .delete(secureRoute, players.delete)
+  .get(players.show)
+  .put(players.update)
+  .delete(players.delete)
 
-router.get('/players/user/:userId', secureRoute, players.indexByUser)
+router.get('/players/user/:userId', players.indexByUser)
 router.post('/register', auth.register)
 router.post('/login', auth.login)
-router.get('/users', secureRoute, auth.index)
+router.get('/users', auth.index)
+
+// Add secureRoute to all routes bar login & register
+// Like below - 
+// .get(secureRoute, players.show)
+// .put(secureRoute, players.update)
 
 export default router
